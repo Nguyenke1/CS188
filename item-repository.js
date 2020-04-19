06227219b05c084cead73d0d9140f0aceaba2b38
Nguyenke1 <60399533+Nguyenke1@users.noreply.github.com>
2020-04-19 13:22:45 -0500

    HW 11 NEXTJS

---
+++ item-repository.js
@@ -0,0 +1,42 @@
+let items = [
+    {
+        'description': 'Drake Sweatpants',
+        'image': 'https://bkstr.scene7.com/is/image/Bkstr/6-33-QZ--H2-C-Black?$GMCategory_ET$',
+        'item_id': 'd83ff143-9f8b-445a-8d8f-b9b8fe0f9f29',
+        'price': 30
+    }
+];
+
+const selectItems = () => ({
+    rows: items
+});
+
+const selectItemByItemId = (itemId) =>
+    items.find((item) => item['item_id'] === itemId);
+
+const insertItem = (item) => items.push(item);
+
+const updateItem = (updatedItem) => {
+    const itemsThatDontMatch = items.filter((item) =>
+        item['item_id'] !== updatedItem['item_id']
+    );
+
+    items = [
+        ...itemsThatDontMatch,
+        updatedItem
+    ];
+};
+
+const deleteItemByItemId = (itemId) => {
+    items = items.filter((item) =>
+        item['item_id'] !== itemId
+    );
+};
+
+module.exports = {
+    deleteItemByItemId,
+    insertItem,
+    selectItemByItemId,
+    selectItems,
+    updateItem
+};
